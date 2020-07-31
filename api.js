5f7752b11f2ceefd882f472f9c2a8097e7251bd9
ksholla20 <ksholla20@gmail.com>
2020-07-31 20:50:35 +0530

    Separated API call to another file

--- api.js
+++ api.js
@@ -1,30 +1,30 @@
 "use strict";
 
 // Imports dependencies
-const request = require("request"),
-  camelCase = require("camelcase"),
+const request = require("request");
       
-function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
+module.exports = class GraphApi {
+    static callSendAPI(sender_psid, response) {
+      // Construct the message body
+      let request_body = {
+        "recipient": {
+          "id": sender_psid
+        },
+        "message": response
+      }
 
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
+      // Send the HTTP request to the Messenger Platform
+      request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+      }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }); 
     }
-  }); 
-}
-
+};
